4d86a4f683565b0bdd467715340c614c79f41590
Burak Mumcu <95939910+BurakMumcu-js@users.noreply.github.com>
2024-01-05 16:47:41 +0300

    imp:ui is forked by codebasics

--- dev/products_dao.py
+++ dev/products_dao.py
@@ -31,12 +31,13 @@ def insert_new_product(connection,product):
 
     return cursor.lastrowid
 
+def delete_product(connection,product_id):
+    cursor = connection.cursor()
+    query = ("DELETE FROM products where product_id=" + str(product_id))
+    cursor.execute(query)
+    connection.commit()
 
 
 if __name__ == '__main__':
     connection = get_sql_connection()
-    print(insert_new_product(connection,{
-        'product_name':'shawarma',
-        'uom_id':'1',
-        'price_per_unit':'45'
-    }))
+    print(delete_product(connection,8))
